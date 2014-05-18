0d5f331d5546605f09a829a760366ad1d1175619
clarkda <dusty@clarkda.com>
2014-05-18 03:18:36 -0500

    reset these, remove debug, write our conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,9 @@ EOF
 # ..
 
 set_env () {
+	location=''
+	forward_to=''
+
 	while [ -z "${location}" ]; do
 	        read -p "location: " temp_location
 	        if [ ! -z ${temp_location} ]; then
@@ -19,7 +22,6 @@ set_env () {
 	while [ -z "${forward_to}" ]; do
 	        read -p "forward to: " temp_forward_to
 	        if [ ! -z ${temp_forward_to} ]; then
-	        	echo temp_forward_to
 	        	forward_to=${temp_forward_to}
 	        fi
 	done
@@ -28,14 +30,17 @@ set_env () {
 }
 
 # ..
+
 build_conf () {
-	awk -v var="\n\tlocation /${location} {\n\t\tproxy_pass ${forward_to};\n\t\tproxy_redirect off;\n\t\tproxy_set_header Host $host;\n\t\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\t}\n" '
+	conf="$(awk -v var="\n\t\tlocation /${location} {\n\t\t\tproxy_pass ${forward_to};\n\t\t\tproxy_redirect off;\n\t\t\tproxy_set_header Host \$host;\n\t\t\tproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;\n\t\t}\n" '
 	/# {{begin}}/{f=1}
 	f {print;print var;f=0;next}
-	1' conf/nginx.conf
+	1' conf/nginx.conf)"
+
+	echo "${conf}" > conf/nginx.conf
 
 	while true; do
-	    read -p $'\n'"add another proxy config? y/n " yn $'\n'
+	    read -p "add another proxy config? y/n " yn
 	    case $yn in
 	        [Yy]* ) set_env; break;;
 	        [Nn]* ) save_env;;
@@ -48,7 +53,7 @@ build_conf () {
 
 save_env () {
 	while true; do
-	    read -p $'\n'"save and build docker image? y/n " yn $'\n'
+	    read -p "save and build docker image? y/n " yn
 	    case $yn in
 	        [Yy]* ) build_image_start_container; break;;
 	        [Nn]* ) exit;;
